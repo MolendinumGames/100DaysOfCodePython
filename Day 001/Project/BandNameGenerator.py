29f840274ea4067937cf18a693140dafffb28870
Christoph <christoph.roemer@yahoo.de>
2023-04-01 15:10:41 +0200

    Day001 added file for daily project

---
+++ Day 001/Project/BandNameGenerator.py
@@ -0,0 +1,10 @@
+
+def main():
+    print("Please enter the name of a city: ")
+    city_name = input()
+    print("Please enter your favorite animal: ")
+    animal_name = input()
+    print("Your band could be called " + city_name + " " + animal_name + "s")
+
+
+main()
