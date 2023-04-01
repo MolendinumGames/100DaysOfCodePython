5d09ee60e3e3be34e4a60aeb0e6003fa1626dc00
Christoph <christoph.roemer@yahoo.de>
2023-04-01 21:14:49 +0200

    Day001 Added comment with alt func in practice file

--- Day 001/Practice Task 01/NameLengthPractice.py
+++ Day 001/Practice Task 01/NameLengthPractice.py
@@ -4,5 +4,8 @@ def main():
     word_length = len(word)
     print('The length of the entered word is: ' + str(word_length))
 
+    # Alternatively:
+    # print('Length of the word is: ' + str(len(input('Please type in a word: '))))
+
 
 main()
