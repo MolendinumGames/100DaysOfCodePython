a31a58e0cf3cd1968043b699c112b889118b7f78
Christoph <christoph.roemer@yahoo.de>
2023-05-01 19:00:17 +0200

    [Day006] Added rest of robot challenges

---
+++ Day006/Hurdles Challenge 4/HurdlesChallenge4.py
@@ -0,0 +1,17 @@
+# Solution to the Hurdles Challenge 4 found here:
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%204&url=worlds%2Ftutorial_en%2Fhurdle4.json
+"""
+def turn_right():
+    for i in range(0,3):
+        turn_left()
+
+
+while at_goal() != True:
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
+"""
