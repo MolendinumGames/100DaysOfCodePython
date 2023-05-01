6888cf4767b24c97cfb69482814431b120964433
Christoph <christoph.roemer@yahoo.de>
2023-05-01 18:32:53 +0200

    [Day006] Added Hurdles Loop Challenge

---
+++ Day006/Hurdles Loop/HurdlesLoops.py
@@ -0,0 +1,23 @@
+# This code is used to solve this challenge:
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
+
+"""
+
+def turn_right():
+    for i in range(0,3):
+        turn_left()
+
+def jump():
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+for i in range(0, 6):
+    move()
+    jump()
+
+"""
